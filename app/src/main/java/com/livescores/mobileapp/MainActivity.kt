99aa961a5e96273e15f1f780534d932eac449189
ardaozyildirim <ardaozyldrm2@gmail.com>
2024-10-17 17:58:25 +0300

    Initial commit

---
+++ app/src/main/java/com/livescores/mobileapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.livescores.mobileapp
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val myWebView = findViewById<WebView>(R.id.myWebView)
+
+        myWebView.apply{
+            loadUrl("https://umutozan.dev")
+            webViewClient = object : WebViewClient(){
+
+            }
+        }
+    }
+}
